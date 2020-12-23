4189311e120139e119ef1ff100ce95d409f09c2f
哲 <hkkb8670@gmail.com>
2020-12-23 18:32:11 +0800

    程式碼更新

---
+++ Assets/testst.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testst : MonoBehaviour
+{
+
+    int a, b;
+    void Start()
+    {
+        a = 5;
+        b = 12;
+        
+        
+        Debug.Log($"OK,{mathTwo(a,b)}");
+    }
+    int mathTwo(int num,int num2)     
+    {
+        int answer = num * num2;
+        return answer;
+    }
+   
+}
